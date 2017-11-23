97dbb6cd867e6dcca4adc3674320378fbf5050a9
Otto Plasmeijer <ottojohan.plasmeijer@coderclass.nl>
2017-11-23 10:32:17 +0100

    1.1: Er is een goed blokje, en er zijn key functions!

--- sketch.js
+++ sketch.js
@@ -1,9 +1,45 @@
+var Player = 25;
+var color = 1;
+
+
+
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  //Canvas van 200 px bij 200 px
+  createCanvas(1525, 710);
+  //Één cijfer geeft grijswaarden
+  background(220);
 }
 
 function draw() {
-  background(255,255,255);
-  drawSprites();
+    //Geen randen om de auto
+    noStroke();
+    //Kies de opvulkleur (rgb)
+    fill(255);
+    //Parameters: x, y, breedte, hoogte
+    rect(Player, 500, 100, 100);
+
+};
+
+function keyPressed() {
+  if(key === 'Z')
+    fill(255, 255, 255);
+  }
+
+function keyPressed() {
+  if(key === 'X')
+    fill(255, 225, 225);
+  }
+
+function keyPressed() {
+    if(keyCode === 32)
+      console.log("Jurjen.")
+      color = 2;
+  }
+
+if (color == 2) {
+  fill(255);
+  //Parameters: x, y, breedte, hoogte
+  rect(Player, 500, 100, 100);
+
 }
