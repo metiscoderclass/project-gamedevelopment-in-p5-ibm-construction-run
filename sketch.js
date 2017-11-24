369e09e846f1c40851cbb866bef66e43ce94c144
Otto Plasmeijer <ottojohan.plasmeijer@coderclass.nl>
2017-11-24 11:58:15 +0100

    1.2: Blokje springt, maar niet zoals wij het willen, en er is bijna een image/sprite

--- sketch.js
+++ sketch.js
@@ -1,45 +1,84 @@
-var Player = 25;
-var color = 1;
-
-
-
+//var breedte = 25;
+//var color = 1;
+//var hoogte = 500;
+//var xBlok = 100;
+//var yBlok = 100;
+var speler;
 
 function setup() {
-  //Canvas van 200 px bij 200 px
   createCanvas(1525, 710);
-  //Één cijfer geeft grijswaarden
   background(220);
+  speler = new BouncingBall(10,10,2,0.10)
+  this.img = loadImage("images/BuildBoyRunFrame1.png");
 }
 
-function draw() {
-    //Geen randen om de auto
-    noStroke();
-    //Kies de opvulkleur (rgb)
-    fill(255);
-    //Parameters: x, y, breedte, hoogte
-    rect(Player, 500, 100, 100);
+function draw () {
+  background(220);
+  speler.display()
+  speler.update()
+}
 
-};
+function BouncingBall (xx, yy, sy, a) {
+  this.x = xx;
+  this.y = yy;
+  this.sy = sy;
+  this.a = a;
 
-function keyPressed() {
-  if(key === 'Z')
-    fill(255, 255, 255);
+  this.update = function () {
+    this.sy += this.a;
+    this.y += this.sy;
+
+    /* botsing tegen de grond */
+    if (this.y > 300) {
+      // this.sy *= -1;
+      this.y = 300;
+      //this.x = random(0,200);
+    }
   }
 
-function keyPressed() {
-  if(key === 'X')
-    fill(255, 225, 225);
+  this.goUp = function () {
+    this.sy = -3;
   }
 
-function keyPressed() {
-    if(keyCode === 32)
-      console.log("Jurjen.")
-      color = 2;
-  }
 
-if (color == 2) {
-  fill(255);
-  //Parameters: x, y, breedte, hoogte
-  rect(Player, 500, 100, 100);
+  this.display = function () {
+    fill (255,255,0);
+    rect(this.x, this.y, 100, 100);
+  }
 
 }
+
+function keyPressed() {
+  if(keyCode === 32)
+    console.log("Jurjen.")
+    speler.goUp()
+  }
+
+//function setup() {
+//  createCanvas(1525, 710);
+//  background(220);
+//}
+//
+//function draw() {
+//    noStroke();
+//    fill(255, 0, 0);
+//    rect(breedte, hoogte, yBlok, xBlok);
+
+//};
+
+//function keyPressed() {
+//  if(key === '90')
+//    fill(255, 255, 255);
+//  }
+
+//function keyPressed() {
+//  if(key === '88')
+//    random(0, 225);
+//  }
+
+//function keyPressed() {
+//    if(keyCode === 32)
+//      console.log("Jurjen.")
+//      hoogte = hoogte - 10
+
+  //}
